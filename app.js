9703b4206c12acea7b64fadb68d91b96e8ded00c
Muhammad Zubair <57016162+MuhammadZubair786@users.noreply.github.com>
2022-11-05 12:14:49 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,77 +1,74 @@
-// // A=4
-// // var inp = parseInt(prompt(""))
 
-// // if(inp==A){
-// //     console.log("Correct")
-// // }
-// // else if(inp+1==A){
-// //     console.log("Close enough to the correct answer")
-// // }
-// // else{
-// //     console.log("Incorrecct")
-// // }
-
-// A= (Math.random()*100).toFixed()
-// alert(A)
-
-// var inp = parseInt(prompt(""))
-
-// if(inp==A){
-//     console.log("Correct")
-// }
-// else if(inp+1==A){
-//     console.log("Close enough to the correct answer")
-// }
-// else{
-//     console.log("Incorrecct")
-// }
-
-// a=prompt()
-// var b =a%3
-// console.log(b)
-// if(b == 0){
-//     console.log("3 is divide")
+// for(i=0;i<10;i++){
+//     if(i<5){
+//         document.write("Start ma sb sei tya <br>")
+//     }
+//     else{
+//         document.write("ab samjha nhi aya rei bhai<br>")
+//     }
 
 // }
-// else{
-//     console.log("not divisible")
+// var alp_array = []//letter
+// var n_list = []//number
+
+// for (var i = 0; i < 5; i++) {
+//     var data = prompt("enter value")
+//     if (data.length > 0) {
+//         if (data.charCodeAt(0) >= 65 && data.charCodeAt(0) <= 90
+//             || data.charCodeAt(0) >= 97 && data.charCodeAt(0) <= 122) {
+//             alp_array.push(data)//alpha_list add 
+//         }
+//         else if (data.charCodeAt(0) >= 48 && data.charCodeAt(0) <= 57) {
+//             n_list.push(data)//number list add 
+//         }
+
+//     }
+//     else {
+//         document.write("No value enter " + "<br>")  
+//     }
+
 // }
 
-// var a1=prompt("Enter Number",5)
-// console.log(a1)
-// if(a1=="" || a1==undefined){
-//     document.write("enter correct data")
-// }
-// else{
-//     document.write("<b class='table'>"+a1+"* "+ 1 +" ="+a1*1)
-//     document.write("<br/>"+a1+"* "+ 2 +" ="+a1*2)
-//     document.write("<br/>"+a1+"* "+ 3 +" ="+a1*3)
-//     document.write("<br/>"+a1+"* "+ 4 +" ="+a1*4)
-//     document.write("<br/>"+a1+"* "+ 5 +" ="+a1*5+"<b>")
+// document.write(alp_array + "<br>")
+// document.write(n_list)
+// document.write(array)
+// console.log(array.length)
+
+// for(var i=0;i<array.length;i++){
+//     document.write(array[i]+"<br>")
 // }
 
-var char = prompt("Enter Character")
-console.log(char.length)
-if(char.length==0){
-    alert("Must Enter One Chaarcter")
-}
-else if(char.length==1){
-    console.log("check character")
-    console.log(char.charCodeAt(0))
-    var getacii = char.charCodeAt(0)
-    if(getacii>=65  && getacii<=90){
-        document.write("Capital Letter Enter")
+
+var even =[]
+var odd=[]
+
+for(var j=0;j<5;j++){
+    var num1 = prompt("Enter Number") //user input
+    if(num1.length>0){
+        //agar user na data enter kiya 
+        if(num1.charCodeAt(0)>=48 && num1.charCodeAt(0)<=57){
+            //check number 
+            if(num1%2==0){
+                // even
+                document.write(num1+" is even number<br>" )
+            }
+            else{
+                //odd
+                document.write(num1+" is odd number<br>")
+
+            }
+        }
+        else{
+            // /ap na number nhi diya
+            document.write("Enter Only number <br>")
+        }
 
     }
-    else if(getacii>=97  && getacii<=122){
-        document.write("small Letter Enter")
-
-    }
-    else if(getacii>=48  && getacii<=57){
-        document.write("number  Enter")
-
+    else{
+        //input hei nhi kiya
+        console.log("Value input must")
     }
 }
-else{
-    console.log("only enter one cha")
-}
+
+
+
