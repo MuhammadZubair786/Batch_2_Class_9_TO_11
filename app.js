0a18c5d44e3917a00dbba6e3d9ac78a54b74dbe9
Muhammad Zubair <57016162+MuhammadZubair786@users.noreply.github.com>
2022-11-06 11:55:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+
+
+// arr[3]
+// for(var i in arr){
+//     // i=>1
+//     // arr[0]=>1
+//     // console.log(arr[i])
+//     if(arr[i]%5!=0){
+//         arr.splice(i,1)//value remove 
+//         console.log(arr)
+//         // [2,5,18,10,15]
+//         i=i-1
+//         // break;
+
+
+//     }
+
+// }
+// var arr =[1,2,25,5,18,10,15,79,50]
+// for(var i=0;i<arr.length;i++){
+//     if(arr[i]%5==0){
+//         console.log("yes")
+//     }
+//     else{
+//         arr.splice(i,1)
+//         console.log(arr)
+//         i=i-1
+//     }
+// }
+
+// var students = ["Asad","basil","kamran","Car","Hadi","daniyal","aqib"];
+
+// for(i=0;i<students.length;i++){
+// //   console.log(students[i].charCodeAt(0))
+//   if(students[i].charCodeAt(0)==65 ||  
+//    students[i].charCodeAt(0)==97||
+//    students[i].charCodeAt(0)==66 ||  
+//    students[i].charCodeAt(0)==98 ||
+//    students[i].charCodeAt(0)==67 ||  
+//    students[i].charCodeAt(0)==99
+//    )  
+//   {
+//     console.log(students[i])
+//   }
+//   else{
+//     // console.log("Remove ",students[i])
+//     students.splice(i,1)
+//     i=i-1
+//   }
+  
+// }
+
+// console.log(students)
+
+arr =[1,2,4,56,78,34,82,45,-3,67]
+lg=arr[0]
+sm= arr[0]
+for(j of arr){
+    if(lg<j){
+        lg=j
+    }
+    if(sm>j){
+        sm=j
+    }
+}
+
+console.log("large : "+lg+"small : "+sm)
+
+// for(j in arr){
+//     console.log(j)//return index 
+//     console.log(arr[j])//return value
+// }
+
+// for( k of arr){
+//     console.log(k)//retunr value 
+
+// }
