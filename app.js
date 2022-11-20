7a67715f83a8fe49b8885cf7ed913ead4c1fa5b7
Muhammad Zubair <57016162+MuhammadZubair786@users.noreply.github.com>
2022-11-20 11:36:18 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,98 +1,77 @@
-// for(var i=0;i<=10;i++){
-//     for(var j=0;j<=10;j++){
-//         if(i==0 || i==10 || j==0 || j==10){
-//         document.write("*")
-//         }
-//         else{
-//             document.write("&nbsp;&nbsp;")
-//         }
-//     }
-//     document.write("<br>")
+// // A=4
+// // var inp = parseInt(prompt(""))
+
+// // if(inp==A){
+// //     console.log("Correct")
+// // }
+// // else if(inp+1==A){
+// //     console.log("Close enough to the correct answer")
+// // }
+// // else{
+// //     console.log("Incorrecct")
+// // }
+
+// A= (Math.random()*100).toFixed()
+// alert(A)
+
+// var inp = parseInt(prompt(""))
+
+// if(inp==A){
+//     console.log("Correct")
+// }
+// else if(inp+1==A){
+//     console.log("Close enough to the correct answer")
+// }
+// else{
+//     console.log("Incorrecct")
 // }
 
-// for(var i=0;i<10;i=i+2){
-//     for(var j=10;j>i;j--){
-//         document.write("&nbsp;")
-//     }
-//     for(var k=i+1;k>0;k--){
-//         document.write("*")
-//     }
-
-//     document.write("<br>")
-// }
-// for(i=10;i>0;i=i-2){
-//     if (i!=10){
-//     for(var j=10;j>=i-1;j--){
-//         document.write("&nbsp")
-//     }
-//     for(var k=i-1;k>0;k--){
-//         document.write("*")
-//     }
-//     document.write("<br>")
-// }
+// a=prompt()
+// var b =a%3
+// console.log(b)
+// if(b == 0){
+//     console.log("3 is divide")
 
 // }
-
-
-// for(var k=i+1;k>0;k--){
-//     document.write("*")
+// else{
+//     console.log("not divisible")
 // }
 
-// for(var a=0;a<10;a++){
-
-//     for(var b=a;b<10;b++){
-//         document.write("&nbsp;&nbsp")
-//     }
-//     for(var c=0;c<a*2-1;c++){
-//         document.write("*")
-//     }
-//     document.write("<br>")
+// var a1=prompt("Enter Number",5)
+// console.log(a1)
+// if(a1=="" || a1==undefined){
+//     document.write("enter correct data")
 // }
-// for(var a=10;a>0;a--){
-
-//     for(var b=a;b<10;b++){
-//         document.write("&nbsp;&nbsp")
-//     }
-//     for(var c=0;c<a*2-1;c++){
-//         document.write("*")
-//     }
-//     document.write("<br>")
+// else{
+//     document.write("<b class='table'>"+a1+"* "+ 1 +" ="+a1*1)
+//     document.write("<br/>"+a1+"* "+ 2 +" ="+a1*2)
+//     document.write("<br/>"+a1+"* "+ 3 +" ="+a1*3)
+//     document.write("<br/>"+a1+"* "+ 4 +" ="+a1*4)
+//     document.write("<br/>"+a1+"* "+ 5 +" ="+a1*5+"<b>")
 // }
 
+var char = prompt("Enter Character")
+console.log(char.length)
+if(char.length==0){
+    alert("Must Enter One Chaarcter")
+}
+else if(char.length==1){
+    console.log("check character")
+    console.log(char.charCodeAt(0))
+    var getacii = char.charCodeAt(0)
+    if(getacii>=65  && getacii<=90){
+        document.write("Capital Letter Enter")
 
-var array = [
-    [1, 2, 6, 3, 6, 8, 9, 6],
-    [4, 5, 6],
-    6,
-    [4, 5, 6, 6],
-    10,
-    [2, 3, 4],
-    6,
-    10
-]
-
-for (var i = 0; i < array.length; i++) {
-    var chk = 0; //chk element 
-
-    if (array[i].length == undefined) {
-        console.log("call")
-        if (array[i] == 6) {
-            document.write((chk + 1 + "<br>"))
-        }
-        else {
-            document.write(0 + "<br>")
-        }
     }
-    else {
-        console.log(array[i].length)
-        for (var j = 0; j < array[i].length; j++) {
-            if (array[i][j] == 6) {
-                chk += 1
-            }
-        }
-        document.write(" <br>" + chk + "<br>")
+    else if(getacii>=97  && getacii<=122){
+        document.write("small Letter Enter")
+
     }
+    else if(getacii>=48  && getacii<=57){
+        document.write("number  Enter")
 
-
-
+    }
+}
+else{
+    console.log("only enter one cha")
 }
