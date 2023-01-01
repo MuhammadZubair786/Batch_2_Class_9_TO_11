757e563b3028da1cc017d9272b04449ecdaa0305
Muhammad Zubair <57016162+MuhammadZubair786@users.noreply.github.com>
2023-01-01 11:30:40 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,107 @@
-// var div = document.getElementsByTagName("div")//main div
-// var arr =[
-//     ["asad","ali","omer"],
-//     ["asad","ali","omer"],
-//     ["asad","ali"],
-//     ["asad","ali","ali"]
-// ]
-// var tb = document.createElement("table")//table create
-// tb.style.border="2px solid black"  
-// tb.style.backgroundColor="grey" 
-// for(var i=0;i<arr.length;i++){
-//     // arr[0][0]
-//     var tr = document.createElement("tr")//create Trow
-//     for(var j=0;j<arr[i].length;j++){
-//         var td = document.createElement("td")//create td 
-//         td.innerText=arr[i][j]
-//         tr.appendChild(td) //tr append td 
-//         td.style.border="1px solid yellow"
-        
-//     }
-//     // td.setAttribute("","background-color: red;")
-//     tb.appendChild(tr) //table append tr
- 
-// }
+// console.log(firebase.database())
+var email = document.getElementById("email")
+var password = document.getElementById("password")
+var signup = document.getElementById("signup")
+var signin = document.getElementById("signin")
+var signout = document.getElementById("signout")
 
-// console.log(tb)
-// div[0].appendChild(tb) //div append table 
+
+signup.addEventListener("click", function () {
+    // console.log(email.value)
+    // console.log(password.value)
+    firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+        .then((user) => {
+            console.log(user.user.uid)
+
+            localStorage.setItem("email",email.value)
+
+            localStorage.setItem("uid",user.user.uid)
+
+            signup.setAttribute("style","display:none")
+            signin.setAttribute("style","display:none")
+
+            var obj = {
+                name : "smit",
+                email:email.value,
+                password:password.value,
+                role: "user",
+                uid:user.user.uid
+            }
+
+            // firebase.database().ref("users/").push(obj)
+            firebase.database().ref("users/").child(user.user.uid).push(obj)
+
+
+            firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+                .then((user) => {
+                    console.log(user.user.uid)
+
+
+
+
+
+                })
+                .catch((err) => {
+                    alert(err.message)
+                })
+
+
+
+        })
+        .catch((e) => {
+            // console.log(console.log(e.message))
+            alert(e.message)
+        })
+})
+
+
+signin.addEventListener("click", function () {
+    firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+        .then((user) => {
+            console.log(user.user)
+            localStorage.setItem("email",email.value)
+
+            localStorage.setItem("uid",user.user.uid)
+
+            signup.setAttribute("style","display:none")
+            signin.setAttribute("style","display:none")
+            signout.removeAttribute("style")
+
+
+
+
+
+
+        })
+        .catch((err) => {
+            alert(err.message)
+        })
+})
+
+var getuid = localStorage.getItem("uid")
+console.log(getuid)
+if(getuid!=null){
+    signup.setAttribute("style","display:none")
+    signin.setAttribute("style","display:none")
+    signout.removeAttribute("style")
+
+}
+
+signout.addEventListener("click",function(){
+    firebase.auth().signOut()
+})
+
+
+
+function Student(name,class1){
+   this.name= name
+   this.class1= class1
+   
+}
+
+var std1 = new Student("asad",13)
+var std2 = new Student("ali")
+
+console.log(std1,std2)
+
+
